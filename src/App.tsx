3aabe12f8936ff543c545bf183ea5c2dbc6fed9b
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-05-02 23:09:35 -0300

    installation of react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
 export const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,11 @@
-export { }
+import { Navigate, Route, Routes } from "react-router-dom"
+
+export const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/pagina-inicial" element={<p>Rotas</p>} />
+
+            <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+        </Routes>
+    )
+}
