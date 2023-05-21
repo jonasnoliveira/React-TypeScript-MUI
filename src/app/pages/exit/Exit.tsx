d1d56c3d8be6efe509057073e290ec71df753e1d
Jonas Nogueira de Oliveira <jonasnoliveirabr@gmail.com>
2023-05-21 01:04:02 -0300

    Update Page Exit

--- src/app/pages/exit/Exit.tsx
+++ src/app/pages/exit/Exit.tsx
@@ -1,9 +1,9 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export const PageExit = () => {
   return (
-    <Box>
-      <h1>Exit</h1>
+    <Box padding={2}>
+      <Typography variant="h4">Exit</Typography>
     </Box>
   );
 };
